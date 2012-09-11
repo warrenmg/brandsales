eec57170f9ffc011fe6c295d0517d8f6f7f6aa21
mw@78e.co.uk <mw@78e.co.uk>
2012-09-11 21:38:10 +0100

    First commit of basic rails shopify app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brandsales::Application.config.secret_token = '7bf3551f26c581e4603e335a6fdac282d2e9609697251d848e1961a937869a7b82167f0a700cb7e3493c7dbc432278c947f07262bf3847aaf93ab052f2c42e65'
