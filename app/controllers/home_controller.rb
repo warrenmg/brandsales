63c746219062776db28814762a45b2958f0242ec
Matt Warren <mw@78e.co.uk>
2012-09-13 17:25:09 +0100

    Figures fetched from shopiy by brand and put into hash

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
   
   around_filter :shopify_session, :except => 'welcome'
   
+  def is_a_number?(s)
+    s.to_s.match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil ? false : true 
+  end
+  
   def welcome
     current_host = "#{request.host}#{':' + request.port.to_s if request.port != 80}"
     @callback_url = "http://#{current_host}/login/finalize"
@@ -9,13 +13,13 @@ class HomeController < ApplicationController
   
   def index
     # get 3 products
-    @products = ShopifyAPI::Product.find(:all, :params => {:limit => 3})
+   # @products = ShopifyAPI::Product.find(:all, :params => {:limit => 3})
 
     # get latest 3 orders
    # @orders   = ShopifyAPI::Order.find(:all, :params => {:limit => 3, :order => "created_at DESC" })
     
-    # get latest 10 orders
-      @orders   = ShopifyAPI::Order.find(:all, :params => {:limit => 10, :fields => "created_at,id,name,total-price,currency,financial_status,billing_address,line_items", :order => "created_at DESC" })
+    # get latest 100 orders
+      @orders   = ShopifyAPI::Order.find(:all, :params => {:limit => 250, :page => 1, :status => "any", :fields => "created_at,id,name,total-price,currency,financial_status,line_items,cancel_reason", :order => "created_at DESC" })
   end
   
 end
