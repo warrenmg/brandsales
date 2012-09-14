ab0ba009b5bf61ab0a37337651ba82a30ac14d5d
mw@78e.co.uk <mw@78e.co.uk>
2012-09-14 18:05:40 +0100

    tidied up report code and added KPI to right slide

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -20,22 +20,42 @@ class HomeController < ApplicationController
     
     # get latest 100 orders
     
-   if params[:orderfilter][:startdate].blank? then
+   if params[:orderfilter].blank? then
            @orderstart = "2012-08-01 00:00"
    else
          @orderstart =  params[:orderfilter][:startdate].to_time.strftime('%Y-%m-%d')
    end
-   if params[:orderfilter][:enddate].blank? then
+   if params[:orderfilter].blank? then
          @orderend= "2015-08-01 00:00"
    else
          @orderend = params[:orderfilter][:enddate].to_time.strftime('%Y-%m-%d')
    end
 
       @orders   = ShopifyAPI::Order.find(:all, :params => {:limit => 250, :created_at_min =>  @orderstart, :created_at_max => @orderend, :page => 1, :status => "any", :fields => "created_at,id,name,total-price,currency,financial_status,line_items,cancel_reason", :order => "created_at DESC" })
-     # @orders   += ShopifyAPI::Order.find(:all, :params => {:limit => 250, :page => 2, :status => "any", :fields => "created_at,id,name,total-price,currency,financial_status,line_items,cancel_reason", :order => "created_at DESC" })
-     # @orders   += ShopifyAPI::Order.find(:all, :params => {:limit => 250, :page => 3, :status => "any", :fields => "created_at,id,name,total-price,currency,financial_status,line_items,cancel_reason", :order => "created_at DESC" })
+      @orders   += ShopifyAPI::Order.find(:all, :params => {:limit => 250, :created_at_min =>  @orderstart, :created_at_max => @orderend, :page => 2, :status => "any", :fields => "created_at,id,name,total-price,currency,financial_status,line_items,cancel_reason", :order => "created_at DESC" })
+      @orders   += ShopifyAPI::Order.find(:all, :params => {:limit => 250, :created_at_min =>  @orderstart, :created_at_max => @orderend, :page => 3, :status => "any", :fields => "created_at,id,name,total-price,currency,financial_status,line_items,cancel_reason", :order => "created_at DESC" })
  
- 
-  end
+      @vend = {"ordercount" => 0, "ordercancelled" => 0}
+
+      @orders.each do |order| 
+
+      if order.financial_status = "authorized" or order.financial_status = "paid"  then 
+      	 if order.cancel_reason.blank? 
+          	   order.line_items.each do |line_item| 
+       			if @vend[line_item.vendor].nil? then
+      				@vend[line_item.vendor] = (line_item.price.to_i * line_item.quantity.to_i)   
+      			else
+      				@vend[line_item.vendor] += (line_item.price.to_i * line_item.quantity.to_i) 
+      		    end
+      			@vend["ordercount"] += line_item.quantity.to_i 
+      		  end 
+      	 else 
+      		@vend["ordercancelled"] += 1
+       	 end 
+         end 
+       end 
+
+      @order_cancel_perc = (@vend["ordercancelled"].to_d / @orders.size.to_d * 100)
+ end
   
 end
