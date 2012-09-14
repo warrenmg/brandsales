024867209541cc9f5849ecc20cc6073a6af01089
mw@78e.co.uk <mw@78e.co.uk>
2012-09-15 00:58:02 +0100

    tided up date validation and added more KPIs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,26 +14,23 @@ class HomeController < ApplicationController
   def index
     # get 3 products
    # @products = ShopifyAPI::Product.find(:all, :params => {:limit => 3})
-
-    # get latest 3 orders
+   # get latest 3 orders
    # @orders   = ShopifyAPI::Order.find(:all, :params => {:limit => 3, :order => "created_at DESC" })
-    
-    # get latest 100 orders
-    
-   if params[:orderfilter].blank? then
-           @orderstart = "2012-08-01 00:00"
-   else
-         @orderstart =  params[:orderfilter][:startdate].to_time.strftime('%Y-%m-%d')
-   end
-   if params[:orderfilter].blank? then
-         @orderend= "2015-08-01 00:00"
-   else
-         @orderend = params[:orderfilter][:enddate].to_time.strftime('%Y-%m-%d')
-   end
 
-      @orders   = ShopifyAPI::Order.find(:all, :params => {:limit => 250, :created_at_min =>  @orderstart, :created_at_max => @orderend, :page => 1, :status => "any", :fields => "created_at,id,name,total-price,currency,financial_status,line_items,cancel_reason", :order => "created_at DESC" })
-      @orders   += ShopifyAPI::Order.find(:all, :params => {:limit => 250, :created_at_min =>  @orderstart, :created_at_max => @orderend, :page => 2, :status => "any", :fields => "created_at,id,name,total-price,currency,financial_status,line_items,cancel_reason", :order => "created_at DESC" })
-      @orders   += ShopifyAPI::Order.find(:all, :params => {:limit => 250, :created_at_min =>  @orderstart, :created_at_max => @orderend, :page => 3, :status => "any", :fields => "created_at,id,name,total-price,currency,financial_status,line_items,cancel_reason", :order => "created_at DESC" })
+   #if params[:enddate].blank? then
+  #       @orderend = Date.today
+  # else
+  #       @orderend = params[:enddate].to_time.strftime('%Y-%m-%d')
+  # end
+   
+   @orderstart = params[:startdate].blank? ?  (Date.today - 7.days) : params[:startdate] 
+   @orderend = params[:enddate].blank? ?  Date.today : params[:enddate] 
+   
+   @orderdiff = (@orderend.to_date - @orderstart.to_date).to_i
+
+      @orders = ShopifyAPI::Order.find(:all, :params => {:limit => 250, :created_at_min =>  @orderstart, :created_at_max => @orderend, :page => 1, :status => "any", :fields => "created_at,id,name,total-price,currency,financial_status,line_items,cancel_reason", :order => "created_at DESC" })
+      @orders += ShopifyAPI::Order.find(:all, :params => {:limit => 250, :created_at_min =>  @orderstart, :created_at_max => @orderend, :page => 2, :status => "any", :fields => "created_at,id,name,total-price,currency,financial_status,line_items,cancel_reason", :order => "created_at DESC" })
+      @orders += ShopifyAPI::Order.find(:all, :params => {:limit => 250, :created_at_min =>  @orderstart, :created_at_max => @orderend, :page => 3, :status => "any", :fields => "created_at,id,name,total-price,currency,financial_status,line_items,cancel_reason", :order => "created_at DESC" })
  
       @vend = {"ordercount" => 0, "ordercancelled" => 0}
 
@@ -56,6 +53,8 @@ class HomeController < ApplicationController
        end 
 
       @order_cancel_perc = (@vend["ordercancelled"].to_d / @orders.size.to_d * 100)
+      @orderactive = (@orders.size - @vend["ordercancelled"]).to_i
+       
  end
   
 end
