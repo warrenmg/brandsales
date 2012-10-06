dcd18a61121f79d87245857042bea669fa1ff04d
umamahesh-nyros <umamahesh_nyros@yahoo.com>
2012-10-06 13:22:02 +0530

    changes for get data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -24,10 +24,11 @@ class HomeController < ApplicationController
   # else
   #       @orderend = params[:enddate].to_time.strftime('%Y-%m-%d')
   # end
-   
-   @orderstart = params[:startdate].blank? ?  (Date.today - 7.days) : params[:startdate] 
-   @orderend = params[:enddate].blank? ?  Date.today : params[:enddate] 
-   
+   # @orderend = params[:enddate].blank? ?  Date.today : params[:enddate] 
+   today = Date.today
+   today.to_s
+   @orderstart = Date.parse "#{today.year}-01-01" 
+   @orderend = Date.today
    @orderdiff = (@orderend.to_date - @orderstart.to_date).to_i
    
 
@@ -40,7 +41,10 @@ class HomeController < ApplicationController
       @orders.size > 1499 ? @orders += ShopifyAPI::Order.find(:all, :params => {:limit => 250, :created_at_min =>  @orderstart, :created_at_max => @orderend, :page => 7, :status => "any", :fields => "created_at,id,name,total-price,currency,financial_status,line_items,cancel_reason", :order => "created_at DESC" }) : "" 
       
       @vend = {"ordercount" => 0, "ordercancelled" => 0, "totalsales" => 0}
+      
       @orders.each do |shop_order| 
+      
+      
       @order= Order.new
       @existing_order = Order.where(:shopify_order_id => shop_order.id, :shopify_name => shop_order.name)
       if @existing_order.blank?
@@ -75,7 +79,7 @@ class HomeController < ApplicationController
       @orderactive = (@orders.size - @vend["ordercancelled"]).to_i
       
        #@o_years = Order.find_by_sql("select DISTINCT(year(order_date)) year_list from orders where shopify_owner= '#{current_shop.url}'")
-       @o_years = Order.find_by_sql("select DISTINCT(extract(year from order_date)) year_list from orders where shopify_owner= '#{current_shop.url}'")
+       @o_years = Order.find_by_sql("select DISTINCT(extract(year from order_date)) as year_list from orders where shopify_owner= '#{current_shop.url}'")
       if @o_years
       @order_year = []
       @o_years.each do |oy|
@@ -141,23 +145,32 @@ class HomeController < ApplicationController
 
 private
 
-def check_orders(year)
+def check_orders(yr)
           
-      @o_years = Order.find_by_sql("select DISTINCT(extract(year from order_date)) year_list from orders where shopify_owner= '#{current_shop.url}'")
+      @o_years = Order.find_by_sql("select DISTINCT(extract(year from order_date))as year_list from orders where shopify_owner= '#{current_shop.url}'")
       @order_year = []
       @o_years.each do |oy|
       @order_year << oy.year_list
       end
+      puts @order_year.inspect
       #params[:year_var]
        puts "############################################################"
       
-      @local_orders = Order.find_by_sql("Select extract(year from order_date) year_list, extract(month from order_date) month_list, vendor_name,sum(price * no_of_items) totCost from orders where shopify_owner = '#{current_shop.url}' and extract(year from order_date) ='#{year}' group by year_list,month_list,vendor_name")
-      puts @local_orders.inspect
+      @local_orders = Order.find_by_sql("Select extract(year from order_date) as year_list, extract(month from order_date) as month_list, vendor_name,sum(price * no_of_items) as totcost from orders where shopify_owner = '#{current_shop.url}' and extract(year from order_date) = #{yr} group by year_list,month_list,vendor_name")
+      @local_orders.each do  |l|
+       puts "############################################################"
+      puts l.inspect
+      puts l.year_list.class
+      puts l.vendor_name
+      puts l.month_list.class
+      puts l.totcost.class
+      
+       puts "############################################################"
+      end
       
       
-       @local_vendors = Order.find_by_sql("select DISTINCT(vendor_name) vendor from orders where shopify_owner= '#{current_shop.url}'")
-       @prices = Array.new
-       @months=["Jan","Feb","Mar","Apr","May","June","July","Aug","Sep","Oct","Nov","Dec"]
+       @local_vendors = Order.find_by_sql("select DISTINCT(vendor_name) as vendor from orders where shopify_owner= '#{current_shop.url}' order by vendor_name"  )
+      @months=["Jan","Feb","Mar","Apr","May","June","July","Aug","Sep","Oct","Nov","Dec"]
        
 end
  
