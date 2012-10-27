2b56af30a4f95c17418c6ece3485d937224e6319
mw@78e.co.uk <mw@78e.co.uk>
2012-10-27 21:33:34 +0100

    Made vendor rows clickable and put into main graph

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,28 @@
 module ApplicationHelper
 def change_to_value(val)
-val.gsub!('"',"")
-val.gsub!('[',"")
-val.gsub!(']',"")
+  val.gsub!('"',"")
+  val.gsub!('[',"")
+  val.gsub!(']',"")
 return val
 end 
+
+def change_to_value_graph(val)
+  val.gsub!('"',"")
+  val.gsub!('[',"")
+  val.gsub!(']',"")
+  val.gsub!(',',"")
+  val.gsub!(@localcurrency,"")
+  if val.blank?
+    val = "0"
+  end
+return val
+end
+
+def url_friendly(val)
+  newvar = val.gsub(/[^a-z0-9]+/i, '-')
+  newvar = newvar.downcase!
+return newvar
+end
+
+
 end
